db1aa9fa8c4ce023f17072efb8525aa23cae6d58
Oussama Ben Saied <ben.saied@proton.me>
2025-01-25 16:52:20 +0100

    fix: app.py deploy error

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import threading
 import pandas as pd
 import cv2
 import numpy as np
-# from flask_cors import CORS
+from flask_cors import CORS
 from flask import Flask, jsonify, request
 import torch
 import torch.nn as nn
@@ -13,7 +13,7 @@ from torchvision import transforms
 port_number = int(os.getenv("PORT", 5000))
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app, origins=["http://localhost:3000"])
 
 # Load the PyTorch model
 class GreenGuruModel(torch.nn.Module):
@@ -78,15 +78,6 @@ def segmentation(image):
 def home():
     return "Welcome to GreenGuru API!"
 
-@app.after_request
-def after_request(response):
-    # Allow all origins
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    # Allow specific headers
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    # Allow specific methods
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    return response
 
 
 # Health check route
